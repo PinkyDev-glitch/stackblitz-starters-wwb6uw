af94851104ab93b6bf859096ead125acc1fe416c
PinkyDev-glitch <137646238+PinkyDev-glitch@users.noreply.github.com>
2023-06-24 23:30:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-wwb6uw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-wwb6uw)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div>
+      {' '}
+      <Message />{' '}
+    </div>
+  );
+}
+export default App;
